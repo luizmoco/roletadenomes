a6ca3c7db8f99735903a4828faf79bd9e8dae52e
Luiz Paulo Lasta Moço <luizpaulolasta@gmail.com>
2025-11-17 09:21:00 -0300

    Merge nomesPadrao and nomesFerias arrays

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const tempoValor = document.getElementById('tempoValor');
 const btnGirar = document.getElementById('btnGirar');
 const btnReset = document.getElementById('btnReset');
 
-const nomesPadrao = ['Anderson', 'Italo', 'Luiz', 'Roberson', 'Henrique', 'Flavio', 'Fernando', 'Rafael'];
-const nomesFerias = ['Thainá'];
+const nomesPadrao = ['Anderson', 'Italo', 'Luiz', 'Roberson', 'Henrique', 'Flavio', 'Fernando', 'Rafael', 'Thainá'];
+const nomesFerias = [];
 const NUM_EXTRA = 5; // checkboxes extras vazios
 
 const CORES_FIXAS = [
